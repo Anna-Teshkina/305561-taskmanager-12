f5419d6f6a2d4b24782970fca0e69d68f575eb91
Anna Teshkina <anna_teshkina@mail.ru>
2020-08-09 18:48:58 +0300

    Добавим функцию по генерации случайного числа из диапазона. Генерируем случайное описание для карточки задачи

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,7 +1,30 @@
 // СГЕНЕРИРУЕМ МОКИ ДЛЯ ОПИСАНИЯ НАШИХ ЗАДАЧ
+
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+// генерируем случайное описание для карточки задачи
+const generateDescription = () => {
+  const descriptions = [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`
+  ];
+
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+
 export const generateTask = () => {
   return {
-    description: null,
+    description: generateDescription(),
     dueDate: null,
     repeating: {
       mo: false,
