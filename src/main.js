08370b4e00fbb474a969357ccda7482ad475efb0
Anna Teshkina <anna_teshkina@mail.ru>
2020-08-26 08:25:55 +0300

    Переименуем renderElement в render, поскольку теперь это единственный метод для отрисовки. renderTemplate пока удалять не будем, он нам еще пригодится.

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import LoadBtnView from "./view/load-btn.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 
-import {renderTemplate, renderElement, RenderPosition} from "./utils.js";
+import {render, RenderPosition} from "./utils.js";
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 // console.log(tasks);
@@ -23,37 +23,37 @@ const filters = generateFilter(tasks);
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-renderElement(siteHeaderElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new SiteFilterView(filters).getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new BoardView().getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new SiteFilterView(filters).getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new BoardView().getElement(), RenderPosition.BEFOREEND);
 
 const boardComponent = new BoardView();
-renderElement(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
-renderElement(boardComponent.getElement(), new SortView().getElement(), RenderPosition.AFTERBEGIN);
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), new SortView().getElement(), RenderPosition.AFTERBEGIN);
 
 const taskListComponent = new TaskListView();
-renderElement(boardComponent.getElement(), taskListComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), taskListComponent.getElement(), RenderPosition.BEFOREEND);
 
-renderElement(taskListComponent.getElement(), new TaskEditView(tasks[0]).getElement(), RenderPosition.BEFOREEND);
+render(taskListComponent.getElement(), new TaskEditView(tasks[0]).getElement(), RenderPosition.BEFOREEND);
 
 // Ограничим первую отрисовку по минимальному количеству,
 // чтобы не пытаться рисовать 8 задач, если всего 5
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderElement(taskListComponent.getElement(), new TaskView(tasks[i]).getElement(), RenderPosition.BEFOREEND);
+  render(taskListComponent.getElement(), new TaskView(tasks[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
 if (tasks.length > TASK_COUNT_PER_STEP) {
   let renderedTaskCount = TASK_COUNT_PER_STEP; // счетчик показанных задач
 
   const loadBtnComponent = new LoadBtnView();
-  renderElement(boardComponent.getElement(), loadBtnComponent.getElement(), RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), loadBtnComponent.getElement(), RenderPosition.BEFOREEND);
 
   // По клику будем допоказывать задачи, опираясь на счётчик
   loadBtnComponent.getElement().addEventListener(`click`, (evt) => {
     evt.preventDefault();
     tasks
       .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderElement(taskListComponent.getElement(), new TaskView(task).getElement(), RenderPosition.BEFOREEND));
+      .forEach((task) => render(taskListComponent.getElement(), new TaskView(task).getElement(), RenderPosition.BEFOREEND));
 
     renderedTaskCount += TASK_COUNT_PER_STEP;
 
