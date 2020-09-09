792031772f84ff8375bf6c5f9cb1f4a824f292ce
Anna Teshkina <anna_teshkina@mail.ru>
2020-09-09 18:19:23 +0300

    Унаследуем простые компоненты (не требующие переопределения конструктора).

--- src/view/load-btn.js
+++ src/view/load-btn.js
@@ -1,29 +1,12 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 // шаблон кнопки загрузки
 export const createLoadBtnTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
 
-export default class LoadBtn {
-  constructor() {
-    this._element = null;
-  }
-
+export default class LoadBtn extends AbstractView {
   getTemplate() {
     return createLoadBtnTemplate();
   }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
-  }
 }
-
