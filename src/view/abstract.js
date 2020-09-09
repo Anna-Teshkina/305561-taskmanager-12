6775f4885844a2835bcd96123b8b4829cafc9929
Anna Teshkina <anna_teshkina@mail.ru>
2020-09-09 18:06:37 +0300

    Выносим общие методы в абстрактный класс: методы getElement и removeElement, свойство _element, метод getTemplate тоже объявим, но в качестве реализации будем бросать исключение, чтобы не забыть его переопределить.

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -1,15 +1,39 @@
-// Добавляем абстрактный класс. 
-// Абстрактный класс нужен, чтобы в одном месте описать общую логику наших компонентов, 
+// Добавляем абстрактный класс.
+// Абстрактный класс нужен, чтобы в одном месте описать общую логику наших компонентов,
 // а после переиспользовать ее благодаря наследованию.
 
-// N.B. Проверка в конструктор е на "new.target" позволит использовать абстрактный класс 
-// только в качестве родительского класса. При поп ытке выполнить "new Abstract()" 
+// N.B. Проверка в конструктор е на "new.target" позволит использовать абстрактный класс
+// только в качестве родительского класса. При поп ытке выполнить "new Abstract()"
 // разработчик получит ошибку.
+
+// Выносим общие методы в абстрактный класс:
+// - Объявим методы getElement и removeElement
+// - Объявим свойство _element
+// - Метод getTemplate тоже объявим, но в качестве реализации будем бросать исключение, чтобы разработчик не забывал его переопределить
+
+import {createElement} from "../utils.js";
 
 export default class Abstract {
   constructor() {
     if (new.target === Abstract) {
       throw new Error(`Can't instantiate Abstract, only concrete one.`);
     }
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method not implemented: getTemplate`);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
