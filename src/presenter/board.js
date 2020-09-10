bd9db3cf27b6f6d81c1fe10286044c6048eeec1b
Anna Teshkina <anna_teshkina@mail.ru>
2020-09-10 15:18:55 +0300

    Рефакторинг логики кнопки допоказа: Локальные переменные перенесем в свойства класса, чтобы к ним был доступ отовсюду. Обработчик клика вынесем в отдельный метод. Пробег по массиву задач с последующим рендерингом заменим на уже описанный метод _renderTasks.

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -14,11 +14,15 @@ const TASK_COUNT_PER_STEP = 8;
 export default class Board {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
+    this._renderedTaskCount = TASK_COUNT_PER_STEP;
 
     this._boardComponent = new BoardView();
     this._sortComponent = new SortView();
     this._taskListComponent = new TaskListView();
     this._noTaskComponent = new NoTaskView();
+    this._loadBtnComponent = new LoadBtnView();
+
+    this._handleLoadBtnClick = this._handleLoadBtnClick.bind(this);
   }
 
   init(boardTasks) {
@@ -85,27 +89,25 @@ export default class Board {
     render(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
 
+  _handleLoadBtnClick() {
+    this._renderTasks(this._renderedTaskCount, this._renderedTaskCount + TASK_COUNT_PER_STEP);
+
+    this._renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    // Если показаны все задачи - скроем кнопку
+    if (this._renderedTaskCount >= this._boardTasks.length) {
+      remove(this._loadBtnComponent);
+    }
+  }
+
   _renderLoadMoreButton() {
     // Метод, куда уйдёт логика по отрисовке компонетов задачи,
     // текущая функция renderTask в main.js
 
-    let renderedTaskCount = TASK_COUNT_PER_STEP; // счетчик показанных задач
-    const loadBtnComponent = new LoadBtnView();
-    render(this._boardComponent, loadBtnComponent, RenderPosition.BEFOREEND);
+    render(this._boardComponent, this._loadBtnComponent, RenderPosition.BEFOREEND);
 
     // По клику будем допоказывать задачи, опираясь на счётчик
-    loadBtnComponent.setClickHandler(() => {
-      this._boardTasks
-        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-        .forEach((boardTask) => this._renderTask(boardTask));
-
-      renderedTaskCount += TASK_COUNT_PER_STEP;
-
-      // Если показаны все задачи - скроем кнопку
-      if (renderedTaskCount >= this._boardTasks.length) {
-        remove(loadBtnComponent);
-      }
-    });
+    this._loadBtnComponent.setClickHandler(this._handleLoadBtnClick);
   }
 
   _renderTaskList() {
